be1f7f4e235fd56f41f39637fdabc80a2907da5e
Peter Nordel <peternordel@gmail.com>
2023-03-20 18:15:14 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return [drivers[0], drivers[1]];
+}
+
+function returnLastTwoDrivers (drivers) {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (multiplier) {
+    return function fareMultiplier (baseFare) {
+        return baseFare * multiplier;
+    }
+}
+
+function fareDoubler (baseFare) {
+    return createFareMultiplier(2)(baseFare);
+}
+
+function fareTripler (baseFare) {
+    return createFareMultiplier(3)(baseFare);
+}
+
+function selectDifferentDrivers(drivers, cb) {
+    return cb(drivers);
+}
